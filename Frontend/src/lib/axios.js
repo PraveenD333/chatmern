0750fe44111ca5bd279fcebaa9117ffc56876fd0
PraveenD333 <praveendhane@gmail.com>
2025-04-17 15:50:09 +0530

    add backend url to frontend

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const axiosInstance = axios.create({
-    baseURL: "http://localhost:3797/api",
+    baseURL: "https://chatmern-rmyi.onrender.com/api",
     withCredentials: true,
     // headers: {
     //     "Content-Type": "application/json",
