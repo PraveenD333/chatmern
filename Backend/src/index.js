fc86aa4a24fc642fdb5fdeb83d1e2f9a2b7e468e
PraveenD333 <praveendhane@gmail.com>
2025-04-17 15:58:23 +0530

    added url vercel

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -13,7 +13,7 @@ const port=process.env.PORT;
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"http://chatmern-six.vercel.app",
     credentials:true,
 }))
 app.use("/api/auth",authRoutes);
